f859e94d4795922cf5635174fec16ded868592fc
Mahalakshmi-Mariappan <mahalakshmi171802@gmail.com>
2024-12-02 14:12:52 +0530

    Update index.js

--- mern-stack-api/index.js
+++ mern-stack-api/index.js
@@ -10,6 +10,10 @@ var postMessageRoutes = require('./controllers/postMessageController')
 var app = express()
 app.use(bodyParser.json())
 app.use(cors({origin:'http://localhost:3000'}))
+// Add a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN Stack API!');
+});
 app.listen(4001,()=>console.log('Server started at : 4001'))
 
 
